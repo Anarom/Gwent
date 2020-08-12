b0d69d8b9cd3c2c508be82c583e56e00798087ba
Anarom <forprojectdi@gmail.com>
2020-08-12 21:12:30 +0300

    Extract data from HTML using BS4

--- parse.py
+++ parse.py
@@ -3,3 +3,9 @@ from bs4 import BeautifulSoup
 
 url = 'http://gwent-cards.com/'
 response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+rows = soup.find_all('tr')[1:]
+for row in rows:
+    cols = row.find_all('td')[1:]  # except first entry which is for image
+    cols = [ele.text.strip() for ele in cols]
+    print(cols)
