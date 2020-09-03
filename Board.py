e1db9c4c1c275995a36bda3f0fe16b4334762d91
Anarom <forprojectdi@gmail.com>
2020-09-03 15:19:43 +0300

    Rewrote board methods with kwargs

--- Board.py
+++ Board.py
@@ -8,29 +8,23 @@ class Army:
     def remove_card(self, card):
         self.rows[card.creature_type].remove_card(card)
 
-    def get_cards_with_max_power(self):
-        max_cards = [row.get_cards_with_max_power() for row in self.rows]
-        max_power = max([row[0].get_total_power() for row in max_cards])
-        cards = [row for row in max_cards if row[0] == max_power]
-        return cards
-
     def get_total_power(self):
         return sum([row.get_total_power() for row in self.rows])
 
     def get_max_power(self):
         return max([row.get_max_power() for row in self.rows])
 
-    def get_row(self, row_type):
-        return self.rows[row_type]
+    def get_rows(self, row_type=None):
+        return self.rows[row_type] if row_type else self.rows
 
-    def get_cards(self):
-        return [row.get_cards() for row in self.rows]
+    def get_cards(self, power=None):
+        return [row.get_cards(power) for row in self.rows]
 
 
 class ArmyRow:
     def __init__(self):
         self.cards = []
-        self.horn_active = False
+        self.horn_sources = []
         self.bad_weather = False
 
     def place_card(self, card):
@@ -40,33 +34,20 @@ class ArmyRow:
     def remove_card(self, card):
         self.cards.remove(card)
         card.row = None
+        if card in self.horn_sources:
+            self.horn_sources.remove(card)
 
     def get_total_power(self):
         return sum([card.get_power() for card in self.cards])
 
     def get_max_power(self):
-        return max([card.get_power() for card in self.cards])
+        return max([card.get_power() for card in self.cards if not card.is_hero])
 
-    def get_cards(self):
-        return self.cards
-
-    def get_cards_with_max_power(self):
-        max_power = self.get_max_power()
-        cards = [card for card in self.cards if card.get_power() == max_power]
-        return cards
+    def get_cards(self, power):
+        return [card for card in self.cards if card.get_power() == power]
 
 
 class Board:
     def __init__(self):
         self.armies = [Army(), Army()]
         self.weather = []
-
-
-class Game:
-    def __init__(self, *players):
-        self.board = Board()
-        self.players = players
-        for player_id, player in enumerate(self.players):
-            player.player_id = player_id
-            player.army = self.board.armies[player_id]
-            player.opponent = self.players[1 - player_id]
