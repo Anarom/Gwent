a6a7c3ac366f0359f1d805ad55e98febe086986d
Anarom <forprojectdi@gmail.com>
2020-09-04 13:43:07 +0300

    Move choose method to Player, pass record to Card

--- cardset.py
+++ cardset.py
@@ -1,6 +1,7 @@
 import random
 import json
-from card import AbilityCard, LeaderCard, UnitCard
+from game import Game
+from card import Card, LeaderCard, UnitCard
 import config
 
 
@@ -14,21 +15,15 @@ class CardSet:
     def add_card(self, card):
         self.cards.append(card)
 
-    def draw_card(self, card_id=None):
-        if card_id:
-            card = None
+    def draw_card(self, specific_card = None, card_id=None):
+        if specific_card:
+            card = specific_card
+        elif card_id:
             for card in self.cards:
                 if card.id == card_id:
                     break
         else:
             card = random.choice(self.cards)
-        self.cards.remove(card)
-        return card
-
-    def choose_card(self):
-        self.print()
-        response = int(input()) - 1
-        card = self.cards[response]
         self.cards.remove(card)
         return card
 
@@ -52,13 +47,13 @@ class CardSet:
         print()
 
 
-class GameLib:
+class GameLib(Game):
     def __init__(self):
-        self.factions = ['Northen Relams', 'Nilfgaardian Empire', "Scoia'tael", 'Monsters', 'Neutral']
+
         self.units = {}
         self.leaders = {}
         self.special_cards = []
-        for faction in self.factions:
+        for faction in Game.factions:
             self.units[faction] = []
             self.leaders[faction] = []
         self.import_data()
@@ -68,14 +63,13 @@ class GameLib:
             records = json.load(file)
         for record in records:
             if record['card_type'] < 3:  # card is unit
-                card = UnitCard(record['id'], record['name'], record['faction'], record['abilities'],
-                                record['card_type'], record['power'], record['power_type'])
+                card = UnitCard(record)
                 self.units[record['faction']].append(card)
             elif record['card_type'] == 3:  # card is special
-                card = AbilityCard(record['id'], record['name'], record['faction'], record['abilities'])
+                card = Card(record)
                 self.special_cards.append(card)
             else:  # card is leader
-                card = LeaderCard(record['id'], record['name'], record['faction'], record['abilities'])
+                card = LeaderCard(record)
                 self.leaders[record['faction']].append(card)
 
     def generate_deck(self, faction, s_card_amount=random.randint(0, config.DECK_SPECIAL_CARD_LIMIT)):
