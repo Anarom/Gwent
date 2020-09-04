d4f1e21a0c700175c51957ffa7552bb95c010b2a
Anarom <forprojectdi@gmail.com>
2020-09-04 13:42:14 +0300

    Simplified Card classes

--- card.py
+++ card.py
@@ -2,36 +2,25 @@ import config
 
 
 class Card:
-    def __init__(self, card_id, name, faction):
-        self.id = card_id
-        self.name = name
-        self.faction = faction
+    def __init__(self, record):
+        for key in record:
+            setattr(self, key, record[key])
         self.is_unit = False
 
     def __str__(self):
         return self.name
 
 
-class AbilityCard(Card):
-    def __init__(self, card_id, name, faction, abilities):
-        super().__init__(card_id, name, faction)
-        self.abilities = abilities
+class LeaderCard(Card):
+    def __init__(self, record):
+        super().__init__(record)
+        self.used = False
 
 
-class LeaderCard(AbilityCard):
-    def __init__(self, card_id, name, faction, abilities):
-        super().__init__(card_id, name, faction, abilities)
-        self.is_used = False
-
-
-class UnitCard(AbilityCard):
-    def __init__(self, card_id, name, faction, abilities, unit_type, power, is_hero):
-        super().__init__(card_id, name, faction, abilities)
-        self.power = power
+class UnitCard(Card):
+    def __init__(self, record):
+        super().__init__(record)
         self.is_unit = True
-        self.is_hero = is_hero
-        self.unit_type = unit_type
-
         self.power_bonus = 0
         self.power_multiplier = 1
         self.row = None
